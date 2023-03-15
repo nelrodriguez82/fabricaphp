6612263e0313e87173e74e1c82b9c172154e5482
nelrodriguez82 <82072045+nelrodriguez82@users.noreply.github.com>
2023-03-15 10:56:08 -0500

    CRUD

---
+++ EjemploCRUDPHP/datos/Conexion.php
@@ -0,0 +1,21 @@
+<?php
+$con=Conexion::getConexion();
+class Conexion {
+
+   
+    public static function getConexion() {
+        $conn = null;
+
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=semillero", "root", "");
+            $conn->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            echo 'ERROR: ' . $ex->getMessage();
+        }
+        return $conn;
+    }
+
+}
+
+
+
---
+++ EjemploCRUDPHP/modelo.dto/RolDto.php
@@ -0,0 +1,22 @@
+<?php
+
+    class RolDto {
+
+        private $idRol = 0;
+        private $nombreRol = "";
+       
+
+
+        function getIdRol() { 
+            return $this->idRol; 
+        } 
+        function setIdRol($idRol) { 
+            $this->idRol = $idRol; 
+        } 
+
+       
+
+        function getNombreRol() { return $this->nombreRol; } 
+        function setNombreRol($nombreRol) { $this->nombreRol = $nombreRol;  } 
+        
+    }
